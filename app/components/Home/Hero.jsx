88ab901edfa55c8d168811ba40247eee4ea9be55
Bridget Amana <amanabridget03@gmail.com>
2024-06-14 21:02:45 +0100

    update hero section

--- app/components/Home/Hero.jsx
+++ app/components/Home/Hero.jsx
@@ -3,7 +3,7 @@ import School from "@/public/assets/images/school.jpg";
 
 const Hero = () => {
   return (
-    <section className="mx-auto max-w-screen-xl lg:h-[90vh] px-4 md:px-8 pt-24 pb-8 lg:pb-0 lg:flex ">
+    <section className="mx-auto max-w-screen-xl lg:h-[70vh] px-4 md:px-8 pb-8 lg:pb-0 lg:flex items-center">
       {/* bg-[url('/assets/images/AI-mobile.png')] bg-no-repeat bg-contain lg:bg-none bg-right */}
 
       <div className="lg:space-y-16 space-y-8 flex-none lg:max-w-[440px] text-left">
@@ -17,18 +17,18 @@ const Hero = () => {
             productivity with our comprehensive platform for students and
             educators.
           </p>
-          <button className="text-white bg-academia-primary hover:bg-academia-primary/90 py-3 px-4 btn btn-ghost font-medium rounded-none text-[14px]">
+          <button className="text-white bg-academia-primary hover:bg-academia-primary/90 py-3 px-8 btn btn-ghost font-medium rounded-[24px] text-[14px]">
             Sign Up
           </button>
         </div>
       </div>
 
-      <div className="lg:flex-1 lg:flex hidden text-center mt-4 lg:mt-0 lg:ml-3 max-w-[1080px] w-full">
+      <div className="lg:flex-1 lg:flex hidden text-center mt-4 lg:mt-0 lg:ml-6 max-w-[1080px] w-full">
         <Image
           src={School}
           className="w-full lg:w-full h-ful"
           alt="school"
-          width={1200}
+          width={900}
           height={900}
         />
       </div>

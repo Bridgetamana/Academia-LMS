42e814cb0f7eed6b261d029ca562ef9691ad548a
Bridget Amana <amanabridget03@gmail.com>
2024-06-14 21:22:20 +0100

    update navbar

--- app/components/Home/Hero.jsx
+++ app/components/Home/Hero.jsx
@@ -3,7 +3,7 @@ import School from "@/public/assets/images/school.jpg";
 
 const Hero = () => {
   return (
-    <section className="mx-auto max-w-screen-xl lg:h-[70vh] px-4 md:px-8 pb-8 lg:pb-0 lg:flex items-center">
+    <section className="mx-auto max-w-screen-xl lg:h-[70vh] px-4 pt-12 lg:pt-0 md:px-8 pb-8 lg:pb-0 lg:flex items-center">
       {/* bg-[url('/assets/images/AI-mobile.png')] bg-no-repeat bg-contain lg:bg-none bg-right */}
 
       <div className="lg:space-y-16 space-y-8 flex-none lg:max-w-[440px] text-left">
@@ -17,7 +17,7 @@ const Hero = () => {
             productivity with our comprehensive platform for students and
             educators.
           </p>
-          <button className="text-white bg-academia-primary hover:bg-academia-primary/90 py-3 px-8 btn btn-ghost font-medium rounded-[24px] text-[14px]">
+          <button className="text-white bg-academia-primary hover:bg-academia-primary/90 py-3 px-8 font-medium rounded-[24px] text-[14px]">
             Sign Up
           </button>
         </div>
--- app/components/common/Navbar.jsx
+++ app/components/common/Navbar.jsx
@@ -1,39 +1,24 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
-import { IoClose, IoMenu } from "react-icons/io5";
 import Link from "next/link";
 
 const Navbar = () => {
-  const [state, setState] = useState(false);
 
   return (
-    <nav className="w-full border-b md:border-0 sticky top-0 backdrop-blur-md z-[999]">
-      <div className="items-center max-w-screen-2xl mx-auto md:flex">
+    <nav className="w-full border-b md:border-0 sticky top-0 backdrop-blur-md z-[999] shadow-sm">
+      <div className="items-center max-w-screen-2xl mx-auto flex justify-between p-4 lg:p-0">
         <div className="flex items-center  px-4 md:px-8 justify-between py-3 md:py-5 md:block">
           <Link href="" className="font-semibold text-xl">
             LOGO
           </Link>
-          <div className="md:hidden">
-            <button
-              className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
-              onClick={() => setState(!state)}
-            >
-              {state ? (
-                <IoClose className="h-6 w-6" />
-              ) : (
-                <IoMenu className="h-6 w-6" />
-              )}
-            </button>
-          </div>
         </div>
-        <div className="hidden md:inline-block md:px-8">
+        <div className="md:px-8">
           <Link
             href="/signup"
-            className="py-3 px-4 btn btn-ghost font-medium rounded-none text-[14px] text-white bg-academia-primary hover:bg-academia-primary/90"
+            className="py-2 px-8 btn btn-ghost font-medium text-[14px] text-white bg-academia-primary hover:bg-academia-primary/90 rounded-[24px] "
           >
-            Get Started
+            Sign Up
           </Link>
         </div>
       </div>

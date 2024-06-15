b07811196e6dd0e8cb24c9e5bb62943ba54310dd
Bridget Amana <amanabridget03@gmail.com>
2024-06-15 13:51:34 +0100

    fix: resolve module not found error

--- app/components/common/index.js
+++ app/components/common/index.js
@@ -1,7 +1,6 @@
 export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
 export { default as Nav } from "../Auth/Nav";
-export { default as DashNav } from "../Dashboard/User/DashNav";
-export { default as Rating } from "./Rating";
+// export { default as DashNav } from "../Dashboard/User/DashNav";
 export { default as ScrollButton } from "./ScrollButton";
-export { default as DashboardSider } from "../Dashboard/User/DashboardSider";
+// export { default as DashboardSider } from "../Dashboard/User/DashboardSider";

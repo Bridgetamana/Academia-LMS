8d4fdc05b3c3f640fd691490a9fbb23cbcecc0cd
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-06-16 21:00:36 +0100

    user dashboard pages

--- app/components/Dashboard/User/pages/CalendarPage.jsx
+++ app/components/Dashboard/User/pages/CalendarPage.jsx
@@ -1,14 +1,14 @@
 import { UserDashboardLayout } from "@/app/_layouts";
 import React from "react";
 
-const Dashboard = () => {
+const CalendarPage = () => {
   return (
     <UserDashboardLayout>
       <section className="max-w-[1640px] flex flex-col gap-6 px-6 py-4 pb-8 md:h-screen overflow-y-scroll">
-        <div>Dashboard</div>
+        <div>Calendar Page</div>
       </section>
     </UserDashboardLayout>
   );
 };
 
-export default Dashboard;
+export default CalendarPage;

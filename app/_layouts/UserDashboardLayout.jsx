5098f5c72b13587cd34a8392d5bcc2df9523aa0e
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-06-16 22:54:17 +0100

    updates

--- app/_layouts/UserDashboardLayout.jsx
+++ app/_layouts/UserDashboardLayout.jsx
@@ -2,12 +2,12 @@ import { DashboardSider, DashNav } from "@/app/components/common";
 
 export const UserDashboardLayout = ({ children }) => {
   return (
-    <section className="drawer lg:drawer-open bg-[#DFE9F3] text-black mx-auto max-w-[1640px]">
-      {/* bg-[#FAFAFA] */}
+    <section className="drawer lg:drawer-open bg-[#F2F2F2] text-black mx-auto max-w-[1640px]">
+      {/* bg-[#DFE9F3] */}
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
         <DashNav />
-        {children}
+        <div className="min-h-screen">{children}</div>
       </div>
       <DashboardSider />
     </section>

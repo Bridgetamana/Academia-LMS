a3a1607ce32598cc3260d771cd1a71e7d2a34f9f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-06-16 23:25:19 +0100

    dashboard navbar

--- app/components/Dashboard/User/DashNav.jsx
+++ app/components/Dashboard/User/DashNav.jsx
@@ -1,76 +1,119 @@
-"use client";
-
-import Image from "next/image";
-import { useState } from "react";
-// import Logo from "@/public/assets/images/logo.png";
-import { IoClose, IoMenu } from "react-icons/io5";
+import React from "react";
+import { Dropdown } from "antd";
+import { CgMenuRightAlt } from "react-icons/cg";
+import { IoChatboxEllipses, IoNotifications } from "react-icons/io5";
 import Link from "next/link";
 
-const DashNav = () => {
-  const [state, setState] = useState(false);
+const messages = [
+  {
+    key: "1",
+    label: <p className="">No messages yet</p>,
+  },
+];
 
-  const navigation = [
-    { title: "Home", path: "/" },
-    { title: "About", path: "/about" },
-    { title: "Services", path: "/services" },
-    { title: "Contact", path: "/contact" },
-    { title: "FAQ", path: "/faq" },
-  ];
+const notifications = [
+  {
+    key: "1",
+    label: <p className="">(0) notifications</p>,
+  },
+];
+
+const profile = [
+  {
+    key: "1",
+    label: <Link href="">Profile</Link>,
+  },
+  {
+    key: "2",
+    label: <Link href="">Settings</Link>,
+  },
+  {
+    key: "3",
+    label: <Link href="">Log Out</Link>,
+  },
+];
+
+const DashNav = () => {
+  const getGreeting = () => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      return "Good morning";
+    } else if (currentHour < 18) {
+      return "Good afternoon";
+    } else {
+      return "Good evening";
+    }
+  };
 
   return (
-    <nav className="w-full border-b md:border-0 sticky top-0 backdrop-blur-md z-[999]">
-      <div className="items-center max-w-screen-2xl mx-auto md:flex justify-between">
-        <div className="flex items-center  px-4 md:px-8 justify-between py-3 md:py-5 md:block">
-          <Link
-            href=""
-            className="text-xl font-semibold mx-auto py-2 text-academia-primary-80"
-          >
-            Academia
-          </Link>
-          {/* <Link href="">
-            <Image src={Logo} width={120} height={50} alt="Logo" />
-          </Link> */}
-          <div className="md:hidden">
-            <button
-              className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
-              onClick={() => setState(!state)}
+    <>
+      <nav className="flex gap-6 items-center px-5 py-3 lg:py-5">
+        <label htmlFor="my-drawer-2" className="flex lg:hidden">
+          <CgMenuRightAlt size="25" />
+        </label>
+
+        <div className="w-full flex gap-8 justify-between items-center">
+          <span className="flex flex-col gap-1">
+            <p className="text-[14px] xs:text-base lg:text-xl font-semibold">
+              {getGreeting()}, <span>User</span>
+            </p>
+            <p className="text-sm hidden lg:flex">
+              Welcome back to the world of knowledge!
+            </p>
+          </span>
+
+          <span className="flex gap-4">
+            <Dropdown
+              menu={{
+                items: messages,
+              }}
+              placement="bottomRight"
             >
-              {state ? (
-                <IoClose className="h-6 w-6" />
-              ) : (
-                <IoMenu className="h-6 w-6" />
-              )}
-            </button>
-          </div>
+              <button
+                type="button"
+                className="p-2 bg-white rounded-full text-gray-500 hover:text-academia-primary-80 active:scale-95"
+              >
+                <IoChatboxEllipses className="w-6 h-6" />
+              </button>
+            </Dropdown>
+
+            <Dropdown
+              menu={{
+                items: notifications,
+              }}
+              placement="bottomRight"
+            >
+              <button
+                type="button"
+                className="p-2 bg-white rounded-full text-gray-500 hover:text-academia-primary-80 active:scale-95"
+              >
+                <span className="relative">
+                  <IoNotifications className="w-6 h-6" />
+                  <span className="rounded-full w-2 h-2 badge-primary indicator-item absolute -top-1 right-0" />
+                </span>
+              </button>
+            </Dropdown>
+
+            <Dropdown
+              menu={{
+                items: profile,
+              }}
+              placement="bottomRight"
+            >
+              <button type="button" className="avatar w-10 active:scale-95">
+                <img
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  className="rounded-full"
+                />
+              </button>
+            </Dropdown>
+          </span>
         </div>
-        <div
-          className={`md:px-8 justify-self-center pb-3 md:block md:pb-0 md:mt-0 ${
-            state
-              ? "flex justify-center text-center pt-12 absolute h-screen w-full p-4 backdrop-blur-lg bg-black/70 text-white"
-              : "hidden"
-          }`}
-        >
-          <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-            {navigation.map((item, idx) => {
-              return (
-                <li
-                  key={idx}
-                  className="md:text-[14px] text-[22px] text-white md:text-[#1E1C27] font-medium hover:text-academia-primary duration-150 uppercase"
-                >
-                  <Link
-                    href={item.path}
-                    className="group transition duration-300"
-                  >
-                    {item.title}
-                    <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-academia-primary" />
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
-    </nav>
+      </nav>
+      <div className=" border border-gray-200" />
+    </>
   );
 };
+
 export default DashNav;

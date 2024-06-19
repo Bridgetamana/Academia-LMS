a237436236bdcb06afd03e20701022f403ab85ca
Bridget Amana <amanabridget03@gmail.com>
2024-06-19 03:29:27 +0100

    update calendar page

--- app/components/Dashboard/Admin/EventCalendarView.jsx
+++ app/components/Dashboard/Admin/EventCalendarView.jsx
@@ -31,7 +31,7 @@ const EventCalendarView = ({ events, onDateClick, onSaveEvent }) => {
   return (
     <div className="flex flex-row h-screen">
       {/* Left: Calendar View */}
-      <div className="w-3/4 p-4 border-r border-gray-200">
+      <div className="w-2/4 p-4 border-r border-gray-200">
         <Calendar
           onChange={() => {}}
           value={new Date()}
@@ -59,7 +59,7 @@ const EventCalendarView = ({ events, onDateClick, onSaveEvent }) => {
       </div>
 
       {/* Right: Sidebar for Event Categories */}
-      <div className="w-1/4 p-4">
+      <div className="w-2/4 p-4">
         <h2 className="text-lg font-semibold mb-4">Upcoming Events</h2>
 
         {/* Assignment Deadlines */}
@@ -67,10 +67,19 @@ const EventCalendarView = ({ events, onDateClick, onSaveEvent }) => {
           <h3 className="text-md font-semibold mb-2">Assignment Deadlines</h3>
           <ul>
             {getEventsByCategory("assignment").map((event, index) => (
-              <li key={index} className="mb-2">
-                <div className="font-semibold">{event.title}</div>
-                <div className="text-sm text-gray-500">{event.date}</div>
-                <div className="text-sm">{event.description}</div>
+              <li
+                key={index}
+                className="mb-2 bg-yellow-200 p-2 border-l-4 border-yellow-700 rounded-sm"
+              >
+                <div className="flex items-center">
+                  <div className="text-sm text-[#62636C] mr-1">
+                    @{event.date},
+                  </div>
+                  <div className="font-semibold text-[#1E1F24]">
+                    {event.title}
+                  </div>
+                </div>
+                <div className="">{event.description}</div>
               </li>
             ))}
           </ul>
@@ -81,10 +90,19 @@ const EventCalendarView = ({ events, onDateClick, onSaveEvent }) => {
           <h3 className="text-md font-semibold mb-2">Class Schedules</h3>
           <ul>
             {getEventsByCategory("classSchedule").map((event, index) => (
-              <li key={index} className="mb-2">
-                <div className="font-semibold">{event.title}</div>
-                <div className="text-sm text-gray-500">{event.date}</div>
-                <div className="text-sm">{event.description}</div>
+              <li
+                key={index}
+                className="mb-2 bg-green-200 p-2 border-l-4 border-green-700 rounded-sm"
+              >
+                <div className="flex items-center">
+                  <div className="text-sm text-[#62636C] mr-1">
+                    @{event.date},
+                  </div>
+                  <div className="font-semibold text-[#1E1F24]">
+                    {event.title}
+                  </div>
+                </div>
+                <div className="">{event.description}</div>
               </li>
             ))}
           </ul>
@@ -95,10 +113,19 @@ const EventCalendarView = ({ events, onDateClick, onSaveEvent }) => {
           <h3 className="text-md font-semibold mb-2">Other Events</h3>
           <ul>
             {getEventsByCategory("otherEvents").map((event, index) => (
-              <li key={index} className="mb-2">
-                <div className="font-semibold">{event.title}</div>
-                <div className="text-sm text-gray-500">{event.date}</div>
-                <div className="text-sm">{event.description}</div>
+              <li
+                key={index}
+                className="mb-2 bg-indigo-200 p-2 border-l-4 border-indigo-700 rounded-sm"
+              >
+                <div className="flex items-center">
+                  <div className="text-sm text-[#62636C] mr-1">
+                    @{event.date},
+                  </div>
+                  <div className="font-semibold text-[#1E1F24]">
+                    {event.title}
+                  </div>
+                </div>
+                <div className="">{event.description}</div>
               </li>
             ))}
           </ul>
